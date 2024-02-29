c4d22f2c15dd2a8725ea2004d1535de532810336
muhammad-ali-2001 <80622150+muhammad-ali-2001@users.noreply.github.com>
2024-02-29 14:46:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+    try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "db0f79a3-6869-42ba-b0da-04ac84c9026f" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  };
+});
+
+app.listen(3001);
+ 
